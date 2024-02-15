52f6d183e3e84d7d9008bdb3ba7d8671e3b85175
AdrMrcs <adrian.villanueva@student.ateneo.edu>
2024-02-15 20:38:50 +0800

    server working. finishing touches needed

--- ledger/views.py
+++ ledger/views.py
@@ -4,16 +4,138 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("HELLO PO")
+    return HttpResponse("Recipe Book")
 
-def recipe_list(request):
+def recipes_list(request):
     ctx = {
-        "tasks":[
-            'Task 1',
-            'Task 2',
-            'Task 3',
-            'Task 4',
-        ]
+    "recipes": [
+        {
+            "name": "Recipe 1",
+            "ingredients": [
+                {
+                    "name": "tomato",
+                    "quantity": "3pcs"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1kg"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1L"
+                },
+                {
+                    "name": "sinigang mix",
+                    "quantity": "1 packet"
+                }
+            ],
+            "link": "/recipe/1"
+        },
+        {
+            "name": "Recipe 2",
+            "ingredients": [
+                {
+                    "name": "garlic",
+                    "quantity": "1 head"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "vinegar",
+                    "quantity": "1/2cup"
+                },
+                {
+                    "name": "water",
+                    "quanity": "1 cup"
+                },
+                {
+                    "name": "salt",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "whole black peppers",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1 kilo"
+                }
+            ],
+            "link": "/recipe/2"
+        }
+    ]
     }
+    return render(request, "recipes_list.html", ctx)
 
-    return render(request, 'recipe_list.html', ctx)
+def recipe_1(request):
+    ctx = {
+    "name": "Recipe 1",
+    "ingredients": [
+        {
+            "name": "tomato",
+            "quantity": "3pcs"
+        },
+        {
+            "name": "onion",
+            "quantity": "1pc"
+        },
+        {
+            "name": "pork",
+            "quantity": "1kg"
+        },
+        {
+            "name": "water",
+            "quantity": "1L"
+        },
+        {
+            "name": "sinigang mix",
+            "quantity": "1 packet"
+        }
+    ],
+    "link": "/recipe/1"
+        
+    }
+    return render(request, "recipe.html", ctx)
+
+def recipe_2(request):
+    ctx ={
+        "name": "Recipe 2",
+        "ingredients": [
+            {
+                "name": "garlic",
+                "quantity": "1 head"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "vinegar",
+                "quantity": "1/2cup"
+            },
+            {
+                "name": "water",
+                "quantity": "1 cup"
+            },
+            {
+                "name": "salt",
+                "quantity": "1 tablespoon"
+            },
+            {
+                "name": "whole black peppers",
+                "quantity": "1 tablespoon"
+            },
+            {
+                "name": "pork",
+                "quantity": "1 kilo"
+            }
+        ],
+        "link": "/recipe/2"
+    }
+    return render(request, 'recipe.html', ctx)
