02780b425d826271292e07854753ccd4a9bd2df2
AdrMrcs <adrian.villanueva@student.ateneo.edu>
2024-03-04 13:25:33 +0800

    added SQL interface

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import *
+
+class RecipeIngredientInLine(admin.TabularInline):
+    model = RecipeIngredient
+    fields = ['ingredient', 'quantity', 'recipe']
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+    search_fields = ('name',)
+    list_display = ('name',)
+
+    inlines = [RecipeIngredientInLine]
+
+admin.site.register(Recipe, RecipeAdmin)
+
 # Register your models here.
